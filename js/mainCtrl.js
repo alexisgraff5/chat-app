172f74e91bd77498c963e3207cc4c0474849287b
alexisgraff5 <lex.graff@gmail.com>
2016-10-26 13:37:07 -0600

    built messageService

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,13 +2,19 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
 
   //The getMessages function will call the getData method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+  $scope.getMessages = function(){
+    
+  }
 
 
 
 
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box), 
+  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
+  $scope.postMessage = function(){
+
+  }
 
 
 
